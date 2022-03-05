a5bd173c6b1d5786a0c6872b2d0dfc85a6be4993
Tom Prince <tprince14@gmail.com>
2022-03-04 21:49:21 -0500

    add netlify

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,6 @@ import { getSpaces } from "../src/data-access/spaces";
 
 // This page will be statically rendered at build time
 export const getStaticProps: GetStaticProps = async () => {
-  const properties = await getProperties();
   const feed = [
     {
       id: 1,
@@ -21,35 +20,20 @@ export const getStaticProps: GetStaticProps = async () => {
       },
     },
   ];
-  return { props: { feed, properties } };
+  return { props: { feed } };
 };
 
 type Props = {
   feed: PostProps[];
-  properties: properties[];
 };
 
 const Blog: React.FC<Props> = (props) => {
-  // Even though this page is statically rendered at build time
-  // This will fetch client side
-  useEffect(() => {
-    Promise.all([
-      fetch("/api/v1/properties").then((res) => res.json()),
-      fetch("/api/v1/spaces").then((res) => res.json()),
-    ]).then(console.log);
-  }, []);
   return (
     <Layout>
       <div className="page">
         <h1>Public Feed</h1>
         <p className="text-blue-600">Hello world!</p>
-        <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
-        </main>
+        <main>Derp</main>
       </div>
       <style jsx>{`
         .post {
